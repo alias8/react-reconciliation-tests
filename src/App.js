1b86bb4fbf39b1efa3ec7ed17350f7ed2ddbae28
James Kirk <jkirk@atlassian.com>
2020-07-01 18:47:46 +1000

    dsada

--- src/App.js
+++ src/App.js
@@ -1,57 +1,64 @@
-import React, {useState, useCallback, useRef} from 'react';
-import {Hello, useCountRenders} from "./Hello";
-import {
-    NonInlineCallback_memo,
-    InlineCallback_memo,
-    NonInlineCallback,
-    InlineCallback,
-    NonInlineCallback_PureComponent
-} from "./components";
+import React, { useState, useCallback } from "react";
+import { Clicker, ClickerMemo } from "./components";
 
 // https://www.youtube.com/watch?v=-Ls48dd-vJE&list=PLN3n1USn4xlmyw3ebYuZmGp60mcENitdM&index=5
 // https://reactjs.org/docs/reconciliation.html
 const App = () => {
-    const [count, setCount] = useState(0)
-    // const renders = useRef(0);
-    // console.log("parent renders: ", ++renders.current)
+  const [sequence, setSequence] = useState("");
+  const letters = ["a", "b", "c", "d"];
+  const increment = useCallback(
+    (n) => {
+      setSequence((c) => {
+        return c + n;
+      });
+    },
+    [setSequence]
+  );
 
-    const favouriteNums = [7];
+  return (
+    <div>
+      <div>sequence: {sequence}</div>
 
-    const increment = useCallback((n) => {
-        setCount(c => c + n)
-    }, [setCount])
+      <div>Buttons with memo:</div>
+      {letters.map((n) => {
+        return (
+          <ClickerMemo
+            key={n}
+            n={n}
+            increment={increment}
+            name={`${n} with memo`}
+            inlineCallback={false}
+          />
+        );
+      })}
 
-    return (
-        <div>
-            <Hello increment={increment}/>
-            <div>count: {count}</div>
+      <div>Buttons with memo:</div>
+      {letters.map((n) => {
+        return (
+          <ClickerMemo
+            key={n}
+            n={n}
+            increment={(n) => increment(n)}
+            name={`${n} with memo and inlineCallback`}
+            inlineCallback={true}
+          />
+        );
+      })}
 
-            <div>increment prop DOES NOT change reference, memo means it will NOT RENDER when App is rendered</div>
-            {favouriteNums.map(n => {
-                return <NonInlineCallback_memo n={n} key={n} increment={increment} name={"NonInlineCallback_memo"}/>
-            })}
-
-            <div>increment prop DOES NOT change reference, PureComponent means it will NOT RENDER when App is rendered</div>
-            {favouriteNums.map(n => {
-                return <NonInlineCallback_PureComponent n={n} key={n} increment={increment} name={"NonInlineCallback_PureComponent"}/>
-            })}
-
-            <div>increment prop DOES NOT change reference, but since not memoized it WILL RENDER when App is rendered</div>
-            {favouriteNums.map(n => {
-                return <NonInlineCallback n={n} key={n} increment={increment} name={"NonInlineCallback"}/>
-            })}
-
-            <div>increment prop DOES change reference, memo makes no difference because it changes each time, it WILL RENDER when App is rendered</div>
-            {favouriteNums.map(n => {
-                return <InlineCallback_memo n={n} key={n} increment={() => increment(n)} name={"InlineCallback_memo"}/>
-            })}
-
-            <div>increment prop DOES change reference, not memoized, it WILL RENDER when App is rendered</div>
-            {favouriteNums.map(n => {
-                return <InlineCallback n={n} key={n} increment={() => increment(n)} name={"InlineCallback"}/>
-            })}
-        </div>
-    );
+      {/*<div>Buttons without memo:</div>*/}
+      {/*{letters.map((n) => {*/}
+      {/*  return (*/}
+      {/*    <Clicker*/}
+      {/*      key={n}*/}
+      {/*      n={n}*/}
+      {/*      increment={increment}*/}
+      {/*      name={`${n} without memo`}*/}
+      {/*      inlineCallback={false}*/}
+      {/*    />*/}
+      {/*  );*/}
+      {/*})}*/}
+    </div>
+  );
 };
 
 export default App;
--- src/Hello.js
+++
@@ -1,8 +0,0 @@
-import React, {useRef} from 'react';
-
-export const Hello = React.memo(({increment}) => {
-    // const renders = useRef(0);
-    // console.log("hello renders: ", ++renders.current)
-    return <button onClick={increment}>hello</button>;
-});
-
